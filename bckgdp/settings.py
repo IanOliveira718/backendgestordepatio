72313e56fc6758d7947592e6e03087838883cd1e
IanOliveira718 <ianoliveira.rj@hotmail.com>
2026-02-18 11:48:05 -0300

    Connection with local Sql server stablished

--- bckgdp/settings.py
+++ bckgdp/settings.py
@@ -73,11 +73,20 @@ WSGI_APPLICATION = 'bckgdp.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "mssql",
+        "NAME": "bdgdp",
+        "HOST": "IOSIL\\SQLEXPRESS01",
+        "OPTIONS": {
+            "driver": "ODBC Driver 18 for SQL Server",
+            "trusted_connection": "yes",
+            "extra_params": "TrustServerCertificate=yes;",
+        },
     }
 }
+
+    # set this to False if you want to turn off pyodbc's connection pooling
+DATABASE_CONNECTION_POOLING = False
 
 
 # Password validation
