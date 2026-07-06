26974697cac60e4c559e37efdf6647d5e68cae5d
Ian Oliveira <93954736+IanOliveira718@users.noreply.github.com>
2026-07-06 13:19:58 -0300

    Change database engine to PostgreSQL
    Updated database configuration to use PostgreSQL.

--- bckgdp/settings.py
+++ bckgdp/settings.py
@@ -59,12 +59,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', 'localhost')]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
